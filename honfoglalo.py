32a9d82211d0336143adf8af70843bb4338fd69b
David Horvath <David Horvath>
2017-04-02 00:14:13 +0200

    Bugfixes

--- honfoglalo.py
+++ honfoglalo.py
@@ -22,18 +22,15 @@ os.system("pause")
 
 while True:
     im = ImageGrab.grab()
-
     im = im.crop((488,342,1100,431))
-
     im = im.convert('LA')
-
-    im.save("temp.png")
-
     ques = unicode(tool.image_to_string(im,lang='hun'))
-
     ques = ques.replace("\n"," ")
-
     print(ques)
+
+    #SELECT * FROM  `honfoglalo` WHERE  `question` LIKE  %kérdés%
+
+    #Check for ''!
 
     ans = unicode(raw_input(), 'cp852')
 
@@ -43,19 +40,7 @@ while True:
 
     db.commit()
 
-    '''try:
-        mydict[txt]
-    except KeyError:
-        print("A válasz nem található az adatbázisban. Helyes válasz:")
-        ans = input()
-        if txt != "" and ans != "":
-            mydict[txt] = ans
-            wr = txt + ";;" + ans + "\n"
-            with open("db.csv", "a") as myfile:
-                myfile.write(wr)
-    else:
-        print(mydict[txt])'''
-
     print(u"Következő kör?")
     if raw_input() != "":
+        db.close()
         break
